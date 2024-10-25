b57d4898552337b8c6b841fd5fd27b36f776efff
gabrieloak123 <gabrielcarvalhopsilva@outlook.com>
2024-10-25 11:40:15 -0300

    curry, from downtown!!!🏀

--- haskell/hw03/ExCurry.hs
+++ haskell/hw03/ExCurry.hs
@@ -1,16 +1,15 @@
 module ExCurry where
 
-import Prelude hiding ( curry , uncurry )
+import Prelude hiding (curry, uncurry)
 
 -- use your mind to infer the types, don't cheat!
 
 -- curry gets a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((α, β) -> γ) -> (α -> β -> γ)
+curry f a b = f (a, b)
 
 -- uncurry gets a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
-
+uncurry :: (α -> β -> γ) -> ((α, β) -> γ)
+uncurry f (a, b) = f a b
